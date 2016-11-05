61e701a7a02eadc2ebc065ccb6f8c088c41d4f10
NingYanGuo <gny315@163.com>
2016-11-05 16:02:34 +0800

    Site updated: 2016-11-05 16:02:33

--- dragBall/Ball.js
+++ dragBall/Ball.js
@@ -6,8 +6,6 @@ function Ball (){
     //初始化 
     this.init = function(container,index){ 
         //var o = document.getElementsByClassName(name)[0]; 
-        //当onmousedown开始拖拽 
-        //o.ondragstart = Ball.start; 
         self.index = index;
         var o = document.createElement('li');
         o.setAttribute('draggable','true');
@@ -21,14 +19,8 @@ function Ball (){
     };
     //设置拖动事件处理程序
     this.start = function(e){ 
-        console.log('start')
-        console.log(e)
-        //self.style = 'opacity:0';
-        //console.log(self.parent)
-        console.log(e.target.parentNode.id)
         var dt = e.dataTransfer;
         dt.setData('Text',e.target.parentNode.id);
         dt.effectAllowed = "move";
-        console.log(dt.getData('Text'))
     };
 };
